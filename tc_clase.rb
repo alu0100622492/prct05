3b763b3f51131343b4a7eb8986c0ad05e3a4c7fa
Noe Campos <alu0100622492@ull.edu.es>
2013-10-18 16:39:37 +0100

    test terminado

--- tc_clase.rb
+++ tc_clase.rb
@@ -7,10 +7,10 @@ class Test_Racional < Test::Unit::TestCase
 
     def test_pruebas
       
-      assert_equal("(16/21)",( Racional.new(2,3).suma(8,4)).to_s)
-      assert_equal("(8/-3)",( Racional.new(2,3).resta(8,4)).to_s)
-      assert_equal("(4/3)",( Racional.new(2,3).producto(8,4)).to_s)
-      assert_equal("(3/1)",( Racional.new(2,3).division(8,4)).to_s)
+      assert_equal("(2/1)",( Racional.new(2,3).suma(6,3)).to_s)
+      assert_equal("(-5/2)",( Racional.new(3,4).resta(1,3)).to_s)
+      assert_equal("(2/9)",( Racional.new(2,3).producto(2,6)).to_s)
+      assert_equal("(12/7)",( Racional.new(2,3).division(8,7)).to_s)
 
     end
 end
